223764605c44471a4d78f25309a7c8639acef061
Pu-Hsuan Wu <hwupu.cs01@nctu.edu.tw>
2024-03-02 19:50:21 +0800

    Add lil-gui

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,43 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import gsap from "gsap";
+import GUI from "lil-gui";
 import { fromEvent, throttle, interval } from "rxjs";
 import "./index.css";
+
+const debugObject: {
+  color: string;
+  subdivision: number;
+  spin?: () => void;
+} = {
+  color: "#ff0000",
+  subdivision: 1,
+};
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl")! as HTMLElement;
 
 // Scene
 const scene = new THREE.Scene();
+
+/**
+ * Random
+ */
+const count = 50;
+const positionsArray = new Float32Array(count * 3 * 3);
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = Math.random() - 0.5;
+}
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+const bufferGeometry = new THREE.BufferGeometry();
+bufferGeometry.setAttribute("position", positionsAttribute);
+const bufferMesh = new THREE.Mesh(
+  bufferGeometry,
+  new THREE.MeshBasicMaterial({ color: 0x550055, wireframe: true }),
+);
+bufferMesh.position.x = 2;
+bufferMesh.position.y = 2;
+scene.add(bufferMesh);
 
 /**
  * Objects
@@ -18,14 +47,14 @@ scene.add(group);
 
 const cube1 = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+  new THREE.MeshBasicMaterial({ color: debugObject.color }),
 );
 cube1.position.set(-2, 0, 0);
 group.add(cube1);
 
 const cube2 = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true }),
 );
 group.add(cube2);
 
@@ -68,6 +97,43 @@ scene.add(camera);
  */
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+
+const gui = new GUI({ width: 320, title: "Lil GUI", closeFolders: true });
+const guiCube1 = gui.addFolder("cube1");
+guiCube1.add(cube1.position, "x").min(-3).max(3).step(0.1).name("cube1.x");
+guiCube1
+  .addColor(debugObject, "color")
+  .name("cube1.color")
+  .onChange((value: string) => {
+    cube1.material.setValues({ color: value });
+  });
+gui.add(bufferMesh, "visible");
+gui.add(bufferMesh.material, "wireframe");
+debugObject.spin = () => {
+  gsap.to(bufferMesh.rotation, { y: bufferMesh.rotation.y + Math.PI * 2 });
+};
+gui.add(debugObject, "spin");
+gui
+  .add(debugObject, "subdivision")
+  .min(1)
+  .max(20)
+  .step(1)
+  .onFinishChange(() => {
+    cube2.geometry.dispose();
+    cube2.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      debugObject.subdivision,
+      debugObject.subdivision,
+      debugObject.subdivision,
+    );
+  });
+gui.hide();
+window.addEventListener("keydown", (event) => {
+  const keyboardEvent = event as KeyboardEvent;
+  if (keyboardEvent.key == "h") gui.show(gui._hidden);
+});
 
 /**
  * Renderer
